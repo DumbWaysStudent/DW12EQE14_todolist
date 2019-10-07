d089c14102ca7196ca46eb31613fc0da71f31a20
syamsul454 <andibrigjent@gmail.com>
2019-10-06 18:37:44 -0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,43 +1,113 @@
 import React, { Component } from 'react';
-import { Container, Header, Left, Body, Right, Button, Icon, Title, Card, Content, Text, CardItem } from 'native-base';
-export default class HeaderExample extends Component {
+import { StyleSheet, State,} from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Card, Content, Text, CardItem, Input, Item } from 'native-base';
 
-  
+const Styles = StyleSheet.create({
+  input : {
+    marginTop: 10,
+    marginBottom: 2,
+    
+  },
+  button : {
+    marginTop: 10,
+    marginBottom: 2,
+    flex: 3
+  },
 
+  chekbox : {
+      marginLeft : 0,
+ },
+  text : {
+    marginLeft : 20,
+  }
+});
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  this.state = {
+      userInput : '',
+      todolist : [],
+      todoList : [],
+     
+
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(text) {
+
+    this.setState({userInput: text });
+  }
+
+  handleSubmit = () => {
+    const todo = this.state.userInput
+    const todos = this.state.todolist
+    this.setState({
+      todolist : todos.concat(todo),
+      userInput : ''
+    })
+  }
+
+  handleRemove = d => {
+    let data = this.state.todolist.splice(d, 1);
+    console.log(d);
+    this.setState({
+      todolist : data
+    })
+  }
+
+ 
   render() {
-  const  activity = ['work', 'swim', 'study', 'sleep', 'run'];
-  const activities = activity.map(function(item){
-    return(  <Card>
+    const items = this.state.todolist.map(function(item,index){
+      return  <Card>
       <CardItem>
-
-        <Text> {item} </Text>
-       
-       </CardItem>
-     </Card> )
-       
       
-      });
 
+        <Text style= {Styles.text}>{item}</Text>
+        <Right>
+           <Button onClick = { item => { this.handleRemove({item})} } ><Text>Hapus</Text></Button> 
+        </Right>
+       </CardItem>
+     </Card>
+    });
     return (
       <Container>
         <Header>
-    
           <Body>
             <Title>Todo List</Title>
           </Body>
+
           <Right>
             <Button transparent>
               <Icon name='menu' />
             </Button>
           </Right>
         </Header>
-
+        <Item style= {Styles.inputUser}>
+        <Left style = {Styles.button}>
+        <Item rounded>
+            <Input onChangeText ={this.handleChange} value= {this.state.userInput} placeholder='Write Todo List'/>
+          </Item>  
+          </Left>
+        <Right style = {Styles.input}>
+        <Button onPress = {this.handleSubmit} rounded>
+            <Text >Submit</Text>
+          </Button>
+          </Right>
+        </Item>
         <Content>
-         {activities}
-          
+         {items}
         </Content>
 
       </Container>
     );
+
+
   }
+
+ 
+
+  
 }
+
